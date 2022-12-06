63d2af83bf5e59e0f710422be765ed5c061e4922
Courtney Mahugu <courtneymahugu@gmail.com>
2022-12-05 23:21:08 -0500

    Was checking to see if the fib function would get called/that the condition works

--- comporg1project.s
+++ comporg1project.s
@@ -7,7 +7,7 @@ main:
     la $Q0 prompt
     syscall
     move $t0 $v0
-    bge $t0 25 fib
+    bge $t0 25 fib_full
     j else
 
 
@@ -20,3 +20,9 @@ else:
 .data
     prompt: .asciiz "Please input a number greater than or equal to 25: "
     errormsg: .asciiz "Illegal number"
+    message: .asciiz "Fib worked"
+
+fib_full:
+    li $v0 4
+    la $a0 message
+    syscall
