bcc857603943249cfbe0c7901a0c43f642bba849
Courtney Mahugu <courtneymahugu@gmail.com>
2022-12-05 23:30:59 -0500

    Trying to call fibonnaci sequence + store answer

--- comporg1project.s
+++ comporg1project.s
@@ -7,8 +7,9 @@ main:
     la $Q0 prompt
     syscall
     move $t0 $v0
-    bge $t0 25 fib_full
-    j else
+    jal fib
+    move $t1 $v0
+    
 
 
 else:
@@ -21,6 +22,12 @@ else:
     prompt: .asciiz "Please input a number greater than or equal to 25: "
     errormsg: .asciiz "Illegal number"
     message: .asciiz "Fib worked"
+
+fib:
+    bge $t0 25 fib_full
+    j else
+    j $ra
+    
 
 fib_full:
     # li $v0 4
